a8ef8faf55f7ab5683a4fef3461e6caf9cfd661c
huaziyishixiyan <huaziyishixiyan@163.com>
2016-07-07 17:27:49 +0800

    Initial Commit

---
+++ 渐变柱状图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  渐变柱状图
+//
+//  Created by 华子 on 16/7/7.
+//  Copyright © 2016年 华子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 渐变柱状图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  渐变柱状图
+//
+//  Created by 华子 on 16/7/7.
+//  Copyright © 2016年 华子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
